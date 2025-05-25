f3e74f096d665b0836e663d79884d561f2fececd
Lixiao-Chinese <1062196871@qq.com>
2025-05-25 15:54:25 +0800

    Add files via upload

---
+++ hash/hashTable/src/main/java/org/example/hashtable/HashTable.java
@@ -0,0 +1,110 @@
+package org.example.hashtable;
+//哈希表
+//疑问：发现位运算可以代替模运算，为什么优先采取位运算，难道是二进制码机器更容易识别吗
+public class HashTable {
+
+    //节点类
+    static class Entry{
+        int hash;//哈希值
+        Object key;//键
+        Object value;//值
+        Entry next;//用于同一个索引下的链表指针
+        //构造方法
+        public Entry(int hash,Object key,Object value){
+            this.hash=hash;
+            this.key=key;
+            this.value=value;
+        }
+    }
+
+    //创建一个节点类型的数组
+    Entry[] table=new Entry[16];
+    //这里的数组长度被要求是2的n次方，他是模运算代替位运算的前提,我们通过模运算(我们用二进制码会发现规律，用位运算取代模运算位运算)找到哈希值跟key之间的关系
+
+    int size=0;//元素个数
+
+    //通过hash,key获取value
+    Object get(int hash, Object key){
+        int index=hash & (table.length-1);//先通过hash码找到对应的索引index
+        //判断数组中当前位置是否为空
+        if(table[index]==null) {
+            //为空，返回空
+            return null;
+        }
+        else {
+            //不为空，找到链表的首节点
+            Entry p=table[index];
+            //遍历链表直到找到，返回value;
+            while(p!=null){
+                if(p.key.equals(key)){
+                    return p;
+                }
+                p=p.next;
+            }
+            //如果没有对应的值，返回空
+            return null;
+        }
+    }
+    //向哈希表中添加一个元素
+    void put(int hash,Object key,Object value){
+        //先找到对应索引
+        int index= hash & (table.length-1);
+        //判断改索引是否为空
+        //为空的话，创建一个节点
+        //不为空的话，将先找到对应节点，然后判断是否有相同key，有的话相当于更新，没有的话，新添
+        if(table[index]==null){
+            //这里需要创建节点吗，直接放进去就可以了吧
+            Entry p=new Entry(hash,key,value);
+            table[index]=p;
+        }
+        else{
+
+            Entry p=table[index];
+            while(true) {
+                if (p.key.equals(key)) {
+                    p.value = value;//更新
+                    return;
+                }
+                if(p.next==null){
+                    break;
+
+                }
+                p=p.next;
+
+            }
+            p.next=new Entry(hash,key,value);
+//            p.next.next=null;
+        }
+        size++;
+    }
+    //通过哈希码以及Key,删除元素，并返回value
+    Object remove(int hash,Object key){
+        int index=hash & (table.length-1);
+        Entry prev=null;//前指针
+        if(table[index]==null){
+            return null;
+        }
+        else{
+            Entry p=table[index];
+            while(p!=null){
+                if(p.key.equals(key)){
+                    //删除节点，要先找到他的前节点
+
+                    //如果是头指针
+                    if(prev==null){
+                        table[index]=p.next;
+                    }
+                    else{
+                        prev.next=p.next;
+                    }
+                    return p.value;
+                }
+                prev=p.next;
+                p=p.next;
+
+            }
+            size--;
+        }
+        return null;
+    }
+}
---
+++ hash/hashTable/src/test/java/org/example/hashtable/HashTableApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hashtable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashTableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
